0e39f2c401cf21b73e1337cf3828c51b46c6dd9d
Your Name <you@example.com>
2023-06-09 16:45:11 +0400

    some additions

--- CPP Module 00/ex01/PhoneBook.cpp
+++ CPP Module 00/ex01/PhoneBook.cpp
@@ -1,30 +1,43 @@
 #include "PhoneBook.hpp"
+PhoneBook::PhoneBook(){}
+PhoneBook::~PhoneBook(){}
 
 void	PhoneBook::ADD(void)
 {
 	std::string	tmp;
 	if (index == 8)
 		index = 0;
-	std::cout >> "ENTER FIRST NAME: " >> std::endl;
+	std::cout << "ENTER FIRST NAME: " << std::endl;
 	getline(std::cin, tmp);
-	Contact[index].SetFName();
-	std::cout >> "ENTER LAST NAME: " >> std::endl;
+	contact[index].SetFName(tmp);
+	std::cout << "ENTER LAST NAME: " << std::endl;
 	getline(std::cin, tmp);
-	Contact[index].SetLName();
-	std::cout >> "ENTER NICKNAME: " >> std::endl;
+	contact[index].SetLName(tmp);
+	std::cout << "ENTER NICKNAME: " << std::endl;
 	getline(std::cin, tmp);
-	Contact[index].SetNName();
-	std::cout >> "ENTER PHONE NUMBER: " >> std::endl;
+	contact[index].SetNName(tmp);
+	std::cout << "ENTER PHONE NUMBER: " << std::endl;
 	getline(std::cin, tmp);
-	Contact[index].SetPNum();
-	std::cout >> "ENTER DARKEST SECRET: " >> std::endl;
+	contact[index].SetPNum(tmp);
+	std::cout << "ENTER DARKEST SECRET: " << std::endl;
 	getline(std::cin, tmp);
-	Contact[index].SetDSec();
+	contact[index].SetDSec(tmp);
 	// if (!tmp)
 	// 	str::cout << "Must type the first name!" << std::endl;
 }
 
-void	SEARCH(void)
+void	PhoneBook::SEARCH(void)
 {
-	
+	int	i;
+	std::string	tmp;
+	std::cout << "ENTER THE INDEX: " << std::endl;
+	getline(std::cin, tmp);
+	i = std::atoi((char *)tmp);
+	std::cout << i << '|' << contact[i].GetFName() << '|' << contact[i].GetLName() << '|' << contact[i].GetNName() << std::endl;
+		
+}
+
+void	PhoneBook::EXIT(void)
+{
+	exit(0);
 }
