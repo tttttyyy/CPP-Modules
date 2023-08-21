5f3564648b8fedfd52e22fc0ea16e0cd25215045
Tommy <tommy@Tommys-MacBook-Pro.local>
2023-08-21 22:11:03 +0400

    added try catch block 08

--- CPP Module 08/ex00/main.cpp
+++ CPP Module 08/ex00/main.cpp
@@ -2,11 +2,19 @@
 
 int main()
 {
-	std::vector<int> cont;
-	cont.push_back(15);
-	cont.push_back(1);
-	cont.push_back(5);
-	cont.push_back(51);
-	cont.push_back(0);
-	easyfind(cont, 5);
+	try
+	{
+		std::vector<int> cont;
+		cont.push_back(15);
+		cont.push_back(1);
+		cont.push_back(5);
+		cont.push_back(51);
+		cont.push_back(0);
+		easyfind(cont, 6);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
 }
