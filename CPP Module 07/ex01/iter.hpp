11185a661c2519320894fb180c5063db86f5659d
Your Name <you@example.com>
2023-08-21 19:01:32 +0400

    bringing back

--- CPP Module 07/ex01/iter.hpp
+++ CPP Module 07/ex01/iter.hpp
@@ -6,46 +6,10 @@
 //creating 2 templates
 //1 for the actual argument can be array of anything
 //the other one for length can be int double size_t etc
-// template <typename arg, typename len> void iter(arg *p, len l, void (*f)(const arg &))
-// {
-// 	for (len i = 0; i < l; i++)
-// 		f(p[i]);
-// }
-
-// template <typename arg> void iter(arg *p, int l, void (*f)(const arg &))
-// {
-// 	for (int i = 0; i < l; i++)
-// 		f(p[i]);
-// }
-
-
-template <typename len>
-void iter(len *p, int l, void (*f)(const len &))
+template <typename arg> void iter(arg *p, int l, void (*f)(const arg &))
 {
 	for (int i = 0; i < l; i++)
 		f(p[i]);
 }
-
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
-}
-
-// template< typename T >
-// void print( const T& x )
-// {
-//   std::cout << x << std::endl;
-//   return;
-// }
 
 #endif
--- CPP Module 07/ex01/main.cpp
+++ CPP Module 07/ex01/main.cpp
@@ -4,34 +4,24 @@ void print(const arg &x)
 {
 	std::cout << x << std::endl;
 }
-int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
 
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
+int main()
+{
+	int arrI[4] = {0, 1, 2, 3};
+	iter(arrI, 4, &print);
 
-  return 0;
+	std::cout << "********************" << std::endl;
+
+	float arrF[4] = {0.2f, 1.2f, 2.2f, 3.2f};
+	iter(arrF, 4, &print);
+
+	std::cout << "********************" << std::endl;
+
+	char arrC[4] = {'a', 'b', 'c', 'd'};
+	iter(arrC, 4, &print);
+
+	std::cout << "********************" << std::endl;
+
+	std::string arrS[4] = {"Hi", "Cute", "Cucumber", "!"};
+	iter(arrS, 4, &print);
 }
-
-
-// int main()
-// {
-// 	int arrI[4] = {0, 1, 2, 3};
-// 	iter(arrI, 4, &print);
-
-// 	std::cout << "********************" << std::endl;
-
-// 	float arrF[4] = {0.2f, 1.2f, 2.2f, 3.2f};
-// 	iter(arrF, 4, &print);
-
-// 	std::cout << "********************" << std::endl;
-
-// 	char arrC[4] = {'a', 'b', 'c', 'd'};
-// 	iter(arrC, 4, &print);
-
-// 	std::cout << "********************" << std::endl;
-
-// 	std::string arrS[4] = {"Hi", "Cute", "Cucumber", "!"};
-// 	iter(arrS, 4, &print);
-// }
